66e1cbdcffea06c1d40400468086e4c794046333
Ryan Flynn <122565977+ryanflynn-dev@users.noreply.github.com>
2024-02-08 10:48:28 +0000

    phoneNumberChecker

---
+++ phoneNumberValidator/script.js
@@ -0,0 +1,27 @@
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const resultsDiv = document.getElementById("results-div");
+
+checkBtn.addEventListener("click", () => {
+  const number = userInput.value;
+  const isValid = validatePhoneNumber(number);
+  if (number === "") {
+    alert("Please provide a phone number");
+    return;
+  }
+  if (isValid) {
+    resultsDiv.textContent = `Valid US number: ${number}`;
+  } else {
+    resultsDiv.textContent = `Invalid US number: ${number}`;
+  }
+});
+
+function validatePhoneNumber(number) {
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
+  return regex.test(number);
+}
+
+function clearInput() {
+  userInput.value = "";
+  resultsDiv.innerHTML = "";
+}
